51212c66aa3ad6635221107af717dac8f276775a
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2019-01-10 15:29:14 +0100

    Initial Commit

---
+++ miniActors/miniActors.h
@@ -0,0 +1,19 @@
+//
+//  miniActors.h
+//  miniActors
+//
+//  Created by Ognen on 2019-01-10.
+//  Copyright © 2019 Netcetera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for miniActors.
+FOUNDATION_EXPORT double miniActorsVersionNumber;
+
+//! Project version string for miniActors.
+FOUNDATION_EXPORT const unsigned char miniActorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <miniActors/PublicHeader.h>
+
+
